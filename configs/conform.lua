873a95a57e1858997627f584f433a2b5912abd9d
Thanh Tran <trongthanh@gmail.com>
2024-04-08 17:07:24 +0700

    feat: enable FormatDisable[!] and FormatEnable format on save autocommand

--- configs/conform.lua
+++ configs/conform.lua
@@ -23,14 +23,35 @@ local options = {
     python = { "black" },
   },
 
-  format_on_save = {
-    -- These options will be passed to conform.format()
-    timeout_ms = 500,
-    lsp_fallback = true,
-  },
+  format_on_save = function(bufnr)
+    -- Disable with a global or buffer-local variable
+    if vim.g.disable_autoformat or vim.b[bufnr].disable_autoformat then
+      return
+    end
+    return { timeout_ms = 500, lsp_fallback = true }
+  end,
 }
 
 require("conform").setup(options)
+
+vim.api.nvim_create_user_command("FormatDisable", function(args)
+  if args.bang then
+    -- FormatDisable! will disable formatting just for this buffer
+    vim.b.disable_autoformat = true
+  else
+    vim.g.disable_autoformat = true
+  end
+end, {
+  desc = "Disable autoformat-on-save",
+  bang = true,
+})
+
+vim.api.nvim_create_user_command("FormatEnable", function()
+  vim.b.disable_autoformat = false
+  vim.g.disable_autoformat = false
+end, {
+  desc = "Re-enable autoformat-on-save",
+})
 
 -- TODO: reimplement go's organize import
 -- The file ends with '.go'
