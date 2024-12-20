ac23a8f8c5eb41581743501d10569d029315b1b0
Thanh Tran <trongthanh@gmail.com>
2024-12-20 11:36:22 +0700

    feat: add folding assist plugin (use lsp > treesitter > indent)

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -92,6 +92,19 @@ local plugins = {
       "nvim-treesitter/nvim-treesitter-textobjects",
     },
   },
+  {
+    "kevinhwang91/nvim-ufo",
+    event = "VeryLazy",
+    dependencies = { "kevinhwang91/promise-async" },
+    opts = {},
+    init = function()
+      vim.o.fillchars = [[eob: ,fold: ,foldopen:▾,foldsep: ,foldclose:▸]]
+      vim.o.foldcolumn = "0" -- '0' is not bad
+      vim.o.foldlevel = 99 -- Using ufo provider need a large value, feel free to decrease the value
+      vim.o.foldlevelstart = 99
+      vim.o.foldenable = true
+    end,
+  },
 
   {
     "nvim-tree/nvim-tree.lua",
